a35b3f04b89c9e357a719ac49611dbb9db0d7c4e
Incredible-donut <60688827+Incredible-donut@users.noreply.github.com>
2020-06-09 17:39:54 +0300

    Get messages

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-alert('chat');
+//get messages from server
+async function getMessagesFromServer(){
+  var response = await fetch('https://fchatiavi.herokuapp.com/get/Incredibledonutsroom/?offset=0&limit=10');
+  response = response.json();
+  console.log(response);
+}
