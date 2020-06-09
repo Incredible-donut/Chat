fcd0a49f0b6d1dc09dee2e04ca808fece38f4aa4
Incredible-donut <60688827+Incredible-donut@users.noreply.github.com>
2020-06-09 19:18:05 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,15 @@ async function getMessagesFromServer(){
    for (var i = 0; i < response.length; i++) {
    var messageData = response[i];
    console.log(messageData); 
-  } 
-  var message = `    
+       var message = `    
     <div class="container" style="margin-top:30px;">
-      <div><p style="color:white; position:relative; left:-10px; top:17px; background-color:#1C1E25; border-radius:7px; ">Name</p></div>
+      <div><p style="color:white; position:relative; left:-10px; top:17px; background-color:#1C1E25; border-radius:7px; ">${messageData.Name}</p></div>
       <div class="alert alert-danger" role="alert" style="background-color:#212529; color:white; border:solid 1px #DC3545; border-radius:5px;">
-      <p> Message </p>
+      <p> ${messageData.Message} </p>
       </div>
     </div>
 `;
+  } 
+
   messages.innerHTML = message;
 }
