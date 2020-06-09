8715c7cf99ae0cfb0e866a22318ea05a18142933
Incredible-donut <60688827+Incredible-donut@users.noreply.github.com>
2020-06-09 18:03:57 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
+var messages = document.getElementById('messages');
 //get messages from server
 async function getMessagesFromServer(){
+  for (var i = 0; i < response.length; i++){
+   var messageData = response[i];
+   console.log(messageData); 
+  }  
   var response = await fetch('https://fchatiavi.herokuapp.com/get/Incredibledonutsroom/?offset=0&limit=10');
-  response = response.json();
-  console.log(response);
+  response = await response.json();
+  var message = `    <!-- Message -->
+    <div class="container" style="margin-top:30px;">
+      <!-- NICKNAME -->
+      <div><p style="color:white; position:relative; left:-10px; top:17px; background-color:#1C1E25; border-radius:7px; ">Name</p></div>
+      <!-- message text -->
+      <div class="alert alert-danger" role="alert" style="background-color:#212529; color:white; border:solid 1px #DC3545; border-radius:5px;">
+      <p> Message </p>
+      </div>
+    </div>`;
+  messages.innerHTML = message;
 }
