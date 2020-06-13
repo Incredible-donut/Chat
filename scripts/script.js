ebd58df21b356c518ae9a9ea4aa51c38668a24f0
Incredible-donut <60688827+Incredible-donut@users.noreply.github.com>
2020-06-13 15:25:36 +0300

    backload

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,31 @@
 var messages = document.getElementById('messages');
 var sendButton = document.getElementById('send-button');
-var roomChose = document.getElementById('room-input');
-var lastMessages = [];
-
 sendButton.addEventListener('click', sendUserMessage);
 //get messages from server
 window.onload = getMessagesFromServer();
-function roomCheck(){
-var room = roomChose.value;
-if(lastMessages.length < response.length){
-scrollToEnd();
-  }
-lastMessages = response;
-}
-function scrollToEnd(){
- messages.scrollTop = messages.scrollHeight;
-
-}
 async function getMessagesFromServer(){
   
   var response = await fetch('https://fchatiavi.herokuapp.com/get/lightsalmonsroom/?offset=0&limit=100');
   response = await response.json();
-     for (var i = 0; i < response.length; i++) {
-          var messageDataResp = response[i];
-          formMessages(messageDataResp);
-     }
-  setTimeout(getMessagesFromServer, 2000)
+  var allMessagesHTML = '';
+  
+   for (var i = 0; i < response.length; i++) {
+     
+   var messageData = response[i];
+   console.log(messageData); 
+       var message = `    
+    <div class="container" style="margin-top:30px;">
+      <div><p style="color:white; position:relative; left:-10px; top:17px; background-color:#1C1E25; border-radius:7px; "> ${messageData.Name} </p></div>
+      <div class="alert alert-danger" role="alert" style="background-color:#212529; color:white; border:solid 1px #DC3545; border-radius:5px;">
+      <p> ${messageData.Message} </p>
+      </div>
+    </div>
+`
+       allMessagesHTML = allMessagesHTML + message;
+
+    }
+  messages.innerHTML = allMessagesHTML;
+  setTimeout(getMessagesFromServer, 3000)
 }
 
 async function sendUserMessage(){
@@ -39,28 +39,12 @@ async function sendUserMessage(){
   alert("You need to chose a nickname!");
   return;
  }
- 
   await fetch('https://fchatiavi.herokuapp.com/send/lightsalmonsroom/', {
   method: 'POST',
   body: JSON.stringify({
   Name: nicknamevar,
   Message: messagevar
-       })
-     });
+     })
+   });
   getMessagesFromServer();
-  }
-function formMessages(messageData){
-    var allMessagesHTML = '';
-       var message = `    
-    <div class="container" style="margin-top:30px;">
-      <div><p style="color:white; position:relative; left:-10px; top:17px; background-color:#1C1E25; border-radius:7px; "> ${messageData.Name} </p></div>
-      <div class="alert alert-danger" role="alert" style="background-color:#212529; color:white; border:solid 1px #DC3545; border-radius:5px;">
-      <p> ${messageData.Message} </p>
-      </div>
-    </div>
-`
-       allMessagesHTML = allMessagesHTML + message;
-  
-    
-  messages.innerHTML = allMessagesHTML;
 }
