db2f39d03b282d045c7f42cf9b6c3ceddb8348bc
Incredible-donut <60688827+Incredible-donut@users.noreply.github.com>
2020-06-13 13:09:44 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -22,23 +22,7 @@ async function getMessagesFromServer(){
   var response = await fetch('https://fchatiavi.herokuapp.com/get/lightsalmonsroom/?offset=0&limit=100');
   response = await response.json();
   var allMessagesHTML = '';
-  
-   for (var i = 0; i < response.length; i++) {
-     
-   var messageData = response[i];
-   console.log(messageData); 
-       var message = `    
-    <div class="container" style="margin-top:30px;">
-      <div><p style="color:white; position:relative; left:-10px; top:17px; background-color:#1C1E25; border-radius:7px; "> ${messageData.Name} </p></div>
-      <div class="alert alert-danger" role="alert" style="background-color:#212529; color:white; border:solid 1px #DC3545; border-radius:5px;">
-      <p> ${messageData.Message} </p>
-      </div>
-    </div>
-`
-       allMessagesHTML = allMessagesHTML + message;
-  
-    }
-  messages.innerHTML = allMessagesHTML;
+  formMessages();
   setTimeout(getMessagesFromServer, 2000)
 }
 
@@ -63,3 +47,21 @@ async function sendUserMessage(){
      });
   getMessagesFromServer();
   }
+function formMessages(){
+   for (var i = 0; i < response.length; i++) {
+     
+   var messageData = response[i];
+   console.log(messageData); 
+       var message = `    
+    <div class="container" style="margin-top:30px;">
+      <div><p style="color:white; position:relative; left:-10px; top:17px; background-color:#1C1E25; border-radius:7px; "> ${messageData.Name} </p></div>
+      <div class="alert alert-danger" role="alert" style="background-color:#212529; color:white; border:solid 1px #DC3545; border-radius:5px;">
+      <p> ${messageData.Message} </p>
+      </div>
+    </div>
+`
+       allMessagesHTML = allMessagesHTML + message;
+  
+    }
+  messages.innerHTML = allMessagesHTML;
+}
